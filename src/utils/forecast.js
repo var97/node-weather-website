2d0ec2a7189b5e6d80ff61735e72ea5cb8d7d081
Varun Agarwal <varun1997agarwal@gmail.com>
2019-08-19 14:49:39 +0530

    new data in forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to fetch weather API!', undefined)
         }
         else{
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' C degrees out. There is a ' + body.currently.precipProbability + '% chance of rain. The temperature high can be around ' + body.daily.data[0].temperatureHigh + ' C degrees.')
         }
     })
 }
